d1fd43935069cb8c9a9462423e3fdf2d7ba464f2
vedelopment <vedelopment@gmail.com>
2017-01-09 10:02:08 -0800

    switching branches

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -3,3 +3,20 @@ angular.module('libraryApp')
 
 // add your BooksIndexController function here!
 // don't forget $http if you need to make requests
+
+function BooksIndexController($http) {
+  console.log('stuff happens');
+  var vm = this;
+  $http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/books'
+  }).then(getBooksSuccess, getBooksError);
+  function getBooksSuccess(res) {
+    vm.books = res.data.books;
+    console.log('books retrieved!');
+    console.log(res);
+  };
+  function getBooksError(res) {
+    console.log(res, err);
+  };
+}
--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -2,9 +2,59 @@ angular.module('libraryApp')
   .controller('BooksShowController', BooksShowController);
 
 // controller function and dependency injection
-// $routeParams and $location are required for routing stuff 
+// $routeParams and $location are required for routing stuff
 //   - but you might need to add a dependency
-BooksShowController.$inject=['$routeParams', '$location'];   
-function BooksShowController($routeParams, $location) {
+BooksShowController.$inject=['$routeParams', '$location', '$http'];
+function BooksShowController($routeParams, $location, $http) {
+  console.log('bookShow called');
   var vm = this;
+  var bookId = $routeParams.id;
+  vm.bookShow = function() {
+    $http({
+      method: 'GET',
+      url: 'https://super-crud.herokuapp.com/books/' + bookId
+    }).then(getBooksSuccess, getBooksError);
+    function getBooksSuccess(res) {
+      vm.books = res.data;
+      console.log('book retrieved!');
+    };
+    function getBooksError(res) {
+      console.log(res, err);
+    };
+  };
+  vm.bookShow();
+
+  vm.bookUpdate = function(book) {
+    console.log('update called');
+    $http({
+      method: 'PUT',
+      url: 'https://super-crud.herokuapp.com/books/' + bookId
+    }).then(updateBookSuccess, updateBookError);
+    function updateBookSuccess(res) {
+      vm.books = res.data.books;
+      console.log('book updated!');
+      console.log(res);
+      $location.path('/');
+    };
+    function updateBookError(res) {
+      console.log(res, err);
+    };
+  };
+
+  vm.bookDelete = function(book) {
+    console.log('delete called');
+    $http({
+      method: 'DELETE',
+      url: 'https://super-crud.herokuapp.com/books/' + bookId
+    }).then(deleteBooksSuccess, deleteBooksError);
+    function deleteBooksSuccess(res) {
+      vm.books = res.data.books;
+      console.log('book deleted!');
+      console.log(res);
+      $location.path('/');
+    };
+    function deleteBooksError(res) {
+      console.log(res, err);
+    };
+  };
 };
